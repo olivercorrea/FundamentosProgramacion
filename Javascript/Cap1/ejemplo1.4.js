bf7fda162791af3762985f6f32b1d5b196480a44
jomaol <olivercorreacabana@outlook.es>
2024-03-09 19:32:58 -0500

    feat: Add new function 'average' with registration and averages calculation
    Changes registration value and adds new variables c1 to c5.
    Defines a new function 'average' that calculates the average of given numbers.
    Logs registration value, average result, and empty line for better readability.

---
+++ Javascript/Cap1/ejemplo1.4.js
@@ -0,0 +1,17 @@
+let registration = 123456;
+let c1=13, c2=14, c3=15, c4=15, c5=16;
+let reply;
+
+console.log('Final average.');
+
+function average(registration, c1, c2, c3, c4, c5) {
+    reply = (c1 + c2 + c3 + c4, c5) / 5;
+
+    console.log(' ');
+    console.log('Registration: %d', registration);
+    console.log('Average: ', reply);
+    console.log()
+}
+
+average(registration, c1, c2, c3, c4, c5);
+
