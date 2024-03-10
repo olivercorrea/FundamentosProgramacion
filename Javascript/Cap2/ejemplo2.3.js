f0752a8f2885ed1ba2964d9b1205f27c8308aa61
jomaol <olivercorreacabana@outlook.es>
2024-03-10 10:35:41 -0500

    feat: Agregar nueva función para el proceso de aprobación
    Agrega nueva función 'calificación' para verificar si una calificación está aprobada o no.
    Cambio en Javascript/Cap2/ejemplo2.3.js

---
+++ Javascript/Cap2/ejemplo2.3.js
@@ -0,0 +1,12 @@
+console.log('¿Aprobado?');
+
+function calificacion(cal){
+    if (cal > 10.5) {
+        return 'Aprobado';
+    } else {
+        return 'Desaprobado';
+    }
+}
+
+console.log(calificacion(10));
+
