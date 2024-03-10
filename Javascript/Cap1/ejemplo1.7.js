6d8db07de3ffaf2f489489009659578721a54147
jomaol <olivercorreacabana@outlook.es>
2024-03-09 20:59:30 -0500

    feat: Agregar un nuevo script para calcular el área del círculo
    Introduce un nuevo script para calcular el área de un círculo.
    Implementa el cálculo utilizando la constante PI y el radio proporcionado.

---
+++ Javascript/Cap1/ejemplo1.7.js
@@ -0,0 +1,15 @@
+console.log('Area de una circunferencia');
+console.log('');
+
+let radio = 2;
+let area;
+const pi = Math.PI;
+
+function areaCircunferencia(){
+    area = pi * radio**2;
+    console.log(area);
+}
+
+areaCircunferencia();
+
+
