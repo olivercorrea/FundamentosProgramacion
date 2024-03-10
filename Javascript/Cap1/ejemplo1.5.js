705108b650e51d4fb6fb8cd02df895b67cfd95c7
jomaol <olivercorreacabana@outlook.es>
2024-03-09 20:48:14 -0500

    feat: Agregue un nuevo script para calcular el cuadrado y el cubo de un número.
    Agrega una nueva declaración de registro de la consola.
    Define variables 'numero', 'cuadrado' y 'cubo'.
    Calcula el cuadrado y los cubos del número dado usando '**' y 'Math.pow()' respectivamente.
    Registra los valores del cuadrado y del cubo en la consola.

---
+++ Javascript/Cap1/ejemplo1.5.js
@@ -0,0 +1,14 @@
+console.log('Cuadrado y cubo de un número');
+
+let numero = 7;
+let cuadrado;
+let cubo;
+
+cuadrado = numero ** 2;
+cubo = Math.pow(numero, 3);
+
+console.log('')
+
+console.log(`Cuadrado: ${cuadrado}`);
+console.log(`Cubo: ${cubo}`);
+
