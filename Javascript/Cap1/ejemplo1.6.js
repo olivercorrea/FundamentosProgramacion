d91b6e84656ea1db2f34db62027124953e6f6438
jomaol <olivercorreacabana@outlook.es>
2024-03-09 20:54:59 -0500

    feat: Agregar función para calcular el perímetro y el área.
    Agrega dos funciones 'perimetro' y 'area' para calcular el perímetro y el área respectivamente.
    Cambios en Javascript/Cap1/ejemplo1.6.js

---
+++ Javascript/Cap1/ejemplo1.6.js
@@ -0,0 +1,16 @@
+console.log('Perimetro y area');
+console.log('');
+
+let altura = 2;
+let base = 4;
+
+function perimetro() {
+    console.log(2*(altura+base));
+};
+
+function area() {
+    console.log(altura*base);
+};
+
+perimetro();
+area();
